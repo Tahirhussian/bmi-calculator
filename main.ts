cdbd57dc64c4a66e439f1ee82527bc305e0d4a9d
Tahir Hussain <tahirhussainkhattak2758@gmail.com>
2024-04-26 23:30:44 +0500

    bmi calculator

---
+++ main.ts
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+
+const ask = await inquirer.prompt([
+    {
+        name : "waightInKg",
+        type : "number",
+        message : "enter your waight in kg"
+    },
+    {
+        name : "heightInMeters",
+        type : "number",
+        message : "enter your height in meters"
+    }
+])
+
+let bmi = ask.waightInKg / (ask.heightInMeters * ask.heightInMeters)
+console.log(`Your bmi ${bmi}`);
+
+
+
